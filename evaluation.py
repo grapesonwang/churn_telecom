6435d8abae87d01fdd5d006c40a258a3b5081e43
fabienn <fabien.ngo@radialpoint.com>
2015-03-06 18:17:34 -0500

    some changes

--- evaluation.py
+++ evaluation.py
@@ -20,15 +20,17 @@ def deviance_curve(classifier, features, labels, metaparameter_name, param_range
     training_scores_std = np.std(training_scores, axis=1)
     validation_scores_mean = np.mean(validation_scores, axis=1)
     validation_scores_std = np.std(validation_scores, axis=1)
+    sns.set_style("darkgrid")
+    sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
     plt.figure(num=None, figsize=fig_size, dpi=600, facecolor='w', edgecolor='k')
     plt.title("Validation Curve")
     plt.xlabel(metaparameter_name)
     plt.ylabel(metric)
     plt.xlim(np.min(param_range), np.max(param_range))
-    plt.semilogx(param_range, training_scores_mean, label="Training " + metric, color="mediumblue")
+    plt.plot(param_range, training_scores_mean, label="Training " + metric, color="mediumblue")
     plt.fill_between(param_range, training_scores_mean - training_scores_std,
                      training_scores_mean + training_scores_std, alpha=0.2, color="lightskyblue")
-    plt.semilogx(param_range, validation_scores_mean, label="validation " + metric,
+    plt.plot(param_range, validation_scores_mean, label="validation " + metric,
                  color="coral")
     plt.fill_between(param_range, validation_scores_mean - validation_scores_std,
                      validation_scores_mean + validation_scores_std, alpha=0.2, color="lightcoral")
